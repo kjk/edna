59293779c8109754df8ceb23c8d601162046138f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2025-06-21 23:08:51 +0200

    port: Fix bug causing editing to break when there are empty blocks of a language that uses a StreamParser
    https://github.com/kjk/edna/commit/f33be7f3a4b0415be87941cf8510a459ac387a94

--- src/editor/lang-heynote/nested-parser.js
+++ src/editor/lang-heynote/nested-parser.js
@@ -1,7 +1,6 @@
-import { NoteContent, NoteLanguage } from "./parser.terms.js";
-import { getLanguage, langGetParser } from "../languages.js";
-
 import { parseMixed } from "@lezer/common";
+import { getLanguage, langGetParser } from "../languages.js";
+import { NoteContent, NoteLanguage } from "./parser.terms.js";
 
 export function configureNesting() {
   // TODO: would have to by async to implement on-demand loading of parsers
@@ -10,7 +9,12 @@ export function configureNesting() {
     if (id == NoteContent) {
       let noteLang = node.node.parent.firstChild.getChildren(NoteLanguage)[0];
       let langName = input.read(noteLang?.from, noteLang?.to);
-      // console.log("langName:", langName);
+
+      // if the NoteContent is empty, we don't want to return a parser, since that seems to cause an
+      // error for StreamLanguage parsers when the buffer size is large (e.g >300 kb)
+      if (node.node.from == node.node.to) {
+        return null;
+      }
       const lang = getLanguage(langName);
       let res = langGetParser(lang);
       if (res) {
