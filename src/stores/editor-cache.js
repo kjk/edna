e84f67aa6557b77888ea420f87d8138b60396eba
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2025-06-22 15:53:47 +0200

    mostly port editor-cache.js

--- src/stores/editor-cache.js
+++ src/stores/editor-cache.js
@@ -1,227 +1,229 @@
-import { toRaw, watch } from "vue";
-import { defineStore } from "pinia";
 import { NoteFormat } from "../common/note-format";
-
-import { useSettingsStore } from "./settings-store";
-import { useErrorStore } from "./error-store";
-import { useHeynoteStore } from "./heynote-store";
 import { HeynoteEditor } from "../editor/editor";
+import { useErrorStore } from "./error-store.svelte";
+import { useHeynoteStore } from "./heynote-store";
+import { useSettingsStore } from "./settings-store";
 
 const NUM_EDITOR_INSTANCES = 5;
 
-export const useEditorCacheStore = defineStore("editorCache", {
-  state: () => ({
-    editorCache: {
-      lru: [],
-      cache: {},
-      watchHandler: null,
-      themeWatchHandler: null,
-      containerElement: null,
-    },
-  }),
+function watch(source, callback) {
+  // TODO: no-op to satisfy theA API
+}
+class EditorCache {
+  editorCache = $state({
+    lru: [],
+    cache: {},
+    watchHandler: null,
+    themeWatchHandler: null,
+    containerElement: null,
+  });
+  watchHandler = null;
+  themeWatchHandler = null;
 
-  actions: {
-    createEditor(path) {
-      const settingsStore = useSettingsStore();
-      const errorStore = useErrorStore();
-      try {
-        return new HeynoteEditor({
-          element: this.containerElement,
-          path: path,
-          theme: settingsStore.theme,
-          keymap: settingsStore.settings.keymap,
-          emacsMetaKey: settingsStore.settings.emacsMetaKey,
-          showLineNumberGutter: settingsStore.settings.showLineNumberGutter,
-          showFoldGutter: settingsStore.settings.showFoldGutter,
-          bracketClosing: settingsStore.settings.bracketClosing,
-          fontFamily: settingsStore.settings.fontFamily,
-          fontSize: settingsStore.settings.fontSize,
-          indentType: settingsStore.settings.indentType,
-          tabSize: settingsStore.settings.tabSize,
-          defaultBlockToken: settingsStore.settings.defaultBlockLanguage,
-          defaultBlockAutoDetect:
-            settingsStore.settings.defaultBlockLanguageAutoDetect,
-          keyBindings: settingsStore.settings.keyBindings,
-        });
-      } catch (e) {
-        errorStore.addError("Error! " + e.message);
-        throw e;
-      }
-    },
+  createEditor(path) {
+    const settingsStore = useSettingsStore();
+    const errorStore = useErrorStore();
+    try {
+      return new HeynoteEditor({
+        element: this.containerElement,
+        path: path,
+        theme: settingsStore.theme,
+        keymap: settingsStore.settings.keymap,
+        emacsMetaKey: settingsStore.settings.emacsMetaKey,
+        showLineNumberGutter: settingsStore.settings.showLineNumberGutter,
+        showFoldGutter: settingsStore.settings.showFoldGutter,
+        bracketClosing: settingsStore.settings.bracketClosing,
+        fontFamily: settingsStore.settings.fontFamily,
+        fontSize: settingsStore.settings.fontSize,
+        indentType: settingsStore.settings.indentType,
+        tabSize: settingsStore.settings.tabSize,
+        defaultBlockToken: settingsStore.settings.defaultBlockLanguage,
+        defaultBlockAutoDetect:
+          settingsStore.settings.defaultBlockLanguageAutoDetect,
+        keyBindings: settingsStore.settings.keyBindings,
+      });
+    } catch (e) {
+      errorStore.addError("Error! " + e.message);
+      throw e;
+    }
+  }
 
-    getOrCreateEditor(path, updateLru) {
-      if (updateLru) {
-        // move to end of LRU
-        this.editorCache.lru = this.editorCache.lru.filter((p) => p !== path);
-        this.editorCache.lru.push(path);
-      }
-      if (this.editorCache.cache[path]) {
-        return this.editorCache.cache[path];
-      } else {
-        const editor = this.createEditor(path);
-        this.addEditor(path, editor);
-        if (!updateLru) {
-          // if need to add the editor to the LRU, but at the top so that it is the first to be removed
-          this.editorCache.lru.unshift(path);
-        }
-        return editor;
-      }
-    },
-
-    getEditor(path) {
+  getOrCreateEditor(path, updateLru) {
+    if (updateLru) {
       // move to end of LRU
       this.editorCache.lru = this.editorCache.lru.filter((p) => p !== path);
       this.editorCache.lru.push(path);
-      if (this.editorCache.cache[path]) {
-        return this.editorCache.cache[path];
+    }
+    if (this.editorCache.cache[path]) {
+      return this.editorCache.cache[path];
+    } else {
+      const editor = this.createEditor(path);
+      this.addEditor(path, editor);
+      if (!updateLru) {
+        // if need to add the editor to the LRU, but at the top so that it is the first to be removed
+        this.editorCache.lru.unshift(path);
       }
-    },
+      return editor;
+    }
+  }
 
-    addEditor(path, editor) {
-      if (this.editorCache.lru.length >= NUM_EDITOR_INSTANCES) {
-        const pathToFree = this.editorCache.lru.shift();
-        this.freeEditor(pathToFree);
-      }
+  getEditor(path) {
+    // move to end of LRU
+    this.editorCache.lru = this.editorCache.lru.filter((p) => p !== path);
+    this.editorCache.lru.push(path);
+    if (this.editorCache.cache[path]) {
+      return this.editorCache.cache[path];
+    }
+  }
 
-      this.editorCache.cache[path] = editor;
-    },
+  addEditor(path, editor) {
+    if (this.editorCache.lru.length >= NUM_EDITOR_INSTANCES) {
+      const pathToFree = this.editorCache.lru.shift();
+      this.freeEditor(pathToFree);
+    }
 
-    freeEditor(pathToFree) {
-      if (!this.editorCache.cache[pathToFree]) {
-        return;
-      }
-      this.editorCache.cache[pathToFree].destroy();
-      delete this.editorCache.cache[pathToFree];
-      this.editorCache.lru = this.editorCache.lru.filter(
-        (p) => p !== pathToFree,
-      );
-    },
+    this.editorCache.cache[path] = editor;
+  }
 
-    eachEditor(fn) {
-      Object.values(toRaw(this.editorCache.cache)).forEach(fn);
-    },
+  freeEditor(pathToFree) {
+    if (!this.editorCache.cache[pathToFree]) {
+      return;
+    }
+    this.editorCache.cache[pathToFree].destroy();
+    delete this.editorCache.cache[pathToFree];
+    this.editorCache.lru = this.editorCache.lru.filter((p) => p !== pathToFree);
+  }
 
-    clearCache(save = true) {
-      console.log("Clearing editor cache");
-      this.eachEditor((editor) => {
-        editor.destroy((save = save));
-      });
-      this.editorCache.cache = {};
-      this.editorCache.lru = [];
-    },
+  eachEditor(fn) {
+    Object.values(this.editorCache.cache).forEach(fn);
+  }
 
-    onCurrenciesLoaded() {
-      this.eachEditor((editor) => {
-        editor.currenciesLoaded();
-      });
-    },
+  clearCache(save = true) {
+    console.log("Clearing editor cache");
+    this.eachEditor((editor) => {
+      editor.destroy((save = save));
+    });
+    this.editorCache.cache = {};
+    this.editorCache.lru = [];
+  }
 
-    setUp(containerElement) {
-      this.containerElement = containerElement;
-      const settingsStore = useSettingsStore();
-      this.watchHandler = watch(
-        () => settingsStore.settings,
-        (newSettings, oldSettings) => {
-          //console.log("Settings changed (watch)", newSettings, oldSettings)
-          const changedKeys = Object.keys(newSettings).filter(
-            (key) => newSettings[key] !== oldSettings[key],
-          );
+  onCurrenciesLoaded() {
+    this.eachEditor((editor) => {
+      editor.currenciesLoaded();
+    });
+  }
 
-          for (const key of changedKeys) {
-            this.eachEditor((editor) => {
-              switch (key) {
-                case "keymap":
-                case "emacsMetaKey":
-                case "keyBindings":
-                  editor.setKeymap(
-                    newSettings.keymap,
-                    newSettings.emacsMetaKey,
-                    newSettings.keyBindings,
-                  );
-                  break;
-                case "showLineNumberGutter":
-                  editor.setLineNumberGutter(newSettings.showLineNumberGutter);
-                  break;
-                case "showFoldGutter":
-                  editor.setFoldGutter(newSettings.showFoldGutter);
-                  break;
-                case "bracketClosing":
-                  editor.setBracketClosing(newSettings.bracketClosing);
-                  break;
-                case "fontFamily":
-                case "fontSize":
-                  editor.setFont(newSettings.fontFamily, newSettings.fontSize);
-                  break;
-                case "indentType":
-                case "tabSize":
-                  editor.setIndentSettings(
-                    newSettings.indentType,
-                    newSettings.tabSize,
-                  );
-                  break;
-                case "defaultBlockLanguage":
-                case "defaultBlockLanguageAutoDetect":
-                  editor.setDefaultBlockLanguage(
-                    newSettings.defaultBlockLanguage,
-                    newSettings.defaultBlockLanguageAutoDetect,
-                  );
-                  break;
-              }
-            });
-          }
-        },
-      );
+  setUp(containerElement) {
+    this.containerElement = containerElement;
+    const settingsStore = useSettingsStore();
+    this.watchHandler = watch(
+      () => settingsStore.settings,
+      (newSettings, oldSettings) => {
+        //console.log("Settings changed (watch)", newSettings, oldSettings)
+        const changedKeys = Object.keys(newSettings).filter(
+          (key) => newSettings[key] !== oldSettings[key],
+        );
 
-      this.themeWatchHandler = watch(
-        () => settingsStore.theme,
-        (theme) => {
+        for (const key of changedKeys) {
           this.eachEditor((editor) => {
-            editor.setTheme(theme);
+            switch (key) {
+              case "keymap":
+              case "emacsMetaKey":
+              case "keyBindings":
+                editor.setKeymap(
+                  newSettings.keymap,
+                  newSettings.emacsMetaKey,
+                  newSettings.keyBindings,
+                );
+                break;
+              case "showLineNumberGutter":
+                editor.setLineNumberGutter(newSettings.showLineNumberGutter);
+                break;
+              case "showFoldGutter":
+                editor.setFoldGutter(newSettings.showFoldGutter);
+                break;
+              case "bracketClosing":
+                editor.setBracketClosing(newSettings.bracketClosing);
+                break;
+              case "fontFamily":
+              case "fontSize":
+                editor.setFont(newSettings.fontFamily, newSettings.fontSize);
+                break;
+              case "indentType":
+              case "tabSize":
+                editor.setIndentSettings(
+                  newSettings.indentType,
+                  newSettings.tabSize,
+                );
+                break;
+              case "defaultBlockLanguage":
+              case "defaultBlockLanguageAutoDetect":
+                editor.setDefaultBlockLanguage(
+                  newSettings.defaultBlockLanguage,
+                  newSettings.defaultBlockLanguageAutoDetect,
+                );
+                break;
+            }
           });
-        },
-      );
+        }
+      },
+    );
 
-      window.document.addEventListener(
-        "currenciesLoaded",
-        this.onCurrenciesLoaded,
-      );
-    },
+    this.themeWatchHandler = watch(
+      () => settingsStore.theme,
+      (theme) => {
+        this.eachEditor((editor) => {
+          editor.setTheme(theme);
+        });
+      },
+    );
 
-    tearDown() {
-      if (this.watchHandler) {
-        this.watchHandler();
-      }
-      if (this.themeWatchHandler) {
-        this.themeWatchHandler();
-      }
+    window.document.addEventListener(
+      "currenciesLoaded",
+      this.onCurrenciesLoaded,
+    );
+  }
 
-      window.document.removeEventListener(
-        "currenciesLoaded",
-        this.onCurrenciesLoaded,
-      );
+  tearDown() {
+    if (this.watchHandler) {
+      this.watchHandler();
+    }
+    if (this.themeWatchHandler) {
+      this.themeWatchHandler();
+    }
 
-      this.editorCache.lru = [];
-      this.editorCache.cache = {};
-    },
+    window.document.removeEventListener(
+      "currenciesLoaded",
+      this.onCurrenciesLoaded,
+    );
 
-    moveCurrentBlockToOtherEditor(targetPath) {
-      const heynoteStore = useHeynoteStore();
+    this.editorCache.lru = [];
+    this.editorCache.cache = {};
+  }
 
-      const editor = toRaw(this.getEditor(heynoteStore.currentBufferPath));
-      let otherEditor = toRaw(this.getOrCreateEditor(targetPath, false));
-      otherEditor.hide();
+  moveCurrentBlockToOtherEditor(targetPath) {
+    const heynoteStore = useHeynoteStore();
 
-      const content = editor.getActiveBlockContent();
-      otherEditor.contentLoadedPromise.then(() => {
-        otherEditor.appendBlockContent(content);
-        editor.deleteActiveBlock();
+    const editor = this.getEditor(heynoteStore.currentBufferPath);
+    let otherEditor = this.getOrCreateEditor(targetPath, false);
+    otherEditor.hide();
 
-        // add the target buffer to recent buffers so that it shows up at the top of the buffer selector
-        heynoteStore.addRecentBuffer(targetPath);
-        heynoteStore.addRecentBuffer(heynoteStore.currentBufferPath);
-      });
+    const content = editor.getActiveBlockContent();
+    otherEditor.contentLoadedPromise.then(() => {
+      otherEditor.appendBlockContent(content);
+      editor.deleteActiveBlock();
 
-      //console.log("LRU", this.editorCache.lru)
-    },
-  },
-});
+      // add the target buffer to recent buffers so that it shows up at the top of the buffer selector
+      heynoteStore.addRecentBuffer(targetPath);
+      heynoteStore.addRecentBuffer(heynoteStore.currentBufferPath);
+    });
+
+    //console.log("LRU", this.editorCache.lru)
+  }
+}
+
+let editorCache = new EditorCache();
+
+export function useEditorCacheStore() {
+  return editorCache;
+}
