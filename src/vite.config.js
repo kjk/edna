d7bbb247e9fd8bb803e2efaf27129f70c12145f6
Krzysztof Kowalczyk <kkowalczyk@gamil.com>
2025-07-19 14:57:35 +0200

    switch manualChunks rollup setting to a function

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,6 +4,112 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
+/**
+ * @param {string} id
+ */
+function manualChunks(id) {
+  const chunksDef = [
+    ["/@zip.js/zip.js", "zipjs"],
+    ["/prettier/", "prettier"],
+
+    ["/@codemirror/legacy-modes/", "langlegacy"],
+    // ["/@codemirror/lang-rust/", "langrust"],
+    // ["/@codemirror/lang-php/", "langphp"],
+    ["/@codemirror/lang-cpp/", "langcpp"],
+
+    // ["/@codemirror/lang-javascript/", "langjavascript"],
+    // ["/@codemirror/lang-css/", "langcss"],
+    // ["/@codemirror/lang-html/", "langhtml"],
+    // ["/@replit/codemirror-lang-svelte/", "langsvelte"],
+
+    [
+      "/@codemirror/lang-javascript/",
+      "/@codemirror/lang-css/",
+      "/@codemirror/lang-html/",
+      "/@codemirror/lang-vue/",
+      "/@replit/codemirror-lang-svelte/",
+      "langweb",
+    ],
+
+    ["@codemirror/lang-python", "langpython"],
+    // ["@codemirror/lang-java", "langjava"],
+    // ["@codemirror/lang-vue", "langvue"],
+    // ["@codemirror/lang-xml", "langxml"],
+
+    // ["@codemirror/lang-lezer", "langlezer"],
+    ["@codemirror/lang-sql", "langsql"],
+    // ["/@codemirror/lang-json/", "langjson"],
+    ["/@replit/codemirror-lang-csharp/", "langcsharp"],
+
+    [
+      "/markdown-it/",
+      "/entities/",
+      "/linkify-it/",
+      "/mdurl/",
+      "/punycode.js/",
+      "/uc.micro/",
+      "/markdown-it-anchor/",
+      "markdownit",
+    ],
+    ["/highlight.js/", "highlightjs"],
+    // ["/@lezer/", "lezer"],
+  ];
+
+  // for highlight.js, don't create a chunk for one .css file
+  if (id.endsWith(".css")) {
+    return null;
+  }
+
+  for (let def of chunksDef) {
+    let n = def.length;
+    for (let i = 0; i < n - 1; i++) {
+      if (id.includes(def[i])) {
+        return def[n - 1];
+      }
+    }
+  }
+  const noLog = [
+    "elaris/src",
+    "/svelte/src/",
+    "debounce",
+    "/@codemirror/autocomplete/",
+    "/@codemirror/lang-html/",
+    "/@codemirror/lang-markdown/",
+    "/@codemirror/autocomplete/",
+    "@codemirror/search/",
+    "/@codemirror/commands/",
+    "/@codemirror/language/",
+    "/@codemirror/lang-java/",
+    "/@codemirror/lang-json/",
+    "/@codemirror/lang-lezer/",
+    "/@codemirror/lang-php/",
+    "/@codemirror/lang-rust/",
+    "/@codemirror/lang-xml/",
+    "/@codemirror/state/",
+    "/@codemirror/view/",
+    "/@codemirror/language/",
+    "/esm-env/",
+    "/clsx/",
+    "/@marijn/find-cluster-break/",
+    "/overlayscrollbars/",
+    "/style-mod/",
+    "/crelt/",
+    "/w3c-keyname/",
+    "vite/modulepreload-polyfill.js",
+    "__vite-browser-external",
+    "vite/preload-helper.js",
+    "commonjsHelpers.js",
+    "/@lezer/",
+  ];
+  for (let s of noLog) {
+    if (id.includes(s)) {
+      return null;
+    }
+  }
+  console.log(id);
+  return null;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: "./public",
@@ -34,40 +140,7 @@ export default defineConfig({
       output: {
         // Preserve function names in the output
         minifyInternalExports: false,
-        manualChunks: {
-          prettier: [
-            "prettier",
-            "prettier/standalone",
-            "prettier/plugins/estree.mjs",
-            "prettier/plugins/babel.mjs",
-            "prettier/plugins/postcss.mjs",
-            "prettier/plugins/html.mjs",
-            "prettier/plugins/markdown.mjs",
-            "prettier/plugins/typescript.mjs",
-            "prettier/plugins/yaml.mjs",
-          ],
-          langjavascript: ["@codemirror/lang-javascript"],
-          langcpp: ["@codemirror/lang-cpp"],
-          langphp: ["@codemirror/lang-php"],
-          langrust: ["@codemirror/lang-rust"],
-          langlegacy: [
-            "@codemirror/legacy-modes/mode/clojure",
-            "@codemirror/legacy-modes/mode/diff",
-            "@codemirror/legacy-modes/mode/erlang",
-            "@codemirror/legacy-modes/mode/go",
-            "@codemirror/legacy-modes/mode/groovy",
-            "@codemirror/legacy-modes/mode/clike",
-            "@codemirror/legacy-modes/mode/powershell",
-            "@codemirror/legacy-modes/mode/ruby",
-            "@codemirror/legacy-modes/mode/shell",
-            "@codemirror/legacy-modes/mode/swift",
-            "@codemirror/legacy-modes/mode/toml",
-            "@codemirror/legacy-modes/mode/yaml",
-            // "@codemirror/legacy-modes/mode/lua",
-            // "@codemirror/legacy-modes/mode/octave",
-          ],
-          zipjs: ["@zip.js/zip.js"],
-        },
+        manualChunks: manualChunks,
       },
     },
   },
